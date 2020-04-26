af6b454773084d8b7e7dc9c2e4d3bae628d4f08e
lisha141 <63555109+lisha141@users.noreply.github.com>
2020-04-26 20:29:03 +0530

    Update Assignment_script.js

--- Assignment_script.js
+++ Assignment_script.js
@@ -58,7 +58,7 @@ var f=names[i].charAt(0);
   // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
   // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
   // name in the loop.
-  if (l === j) {
+  if (l === "j") {
     console.log("Good Bye "+names[i]);
   } else {
     console.log("Hello "+names[i]);
